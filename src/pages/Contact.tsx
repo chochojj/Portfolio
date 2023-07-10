16aa15c4ec9beda02da97bb975453eae6d222354
chochojj <csgy114@gmail.com>
2023-07-11 00:08:02 +0900

    design: 컨텍트 레이아웃 작성

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 const Contact = () => {
   return (
-  <Wrap>
-    <Title>
-      <Text>Contact</Text>
-      <Line />
-    </Title>
-  </Wrap>
+    <Wrap>
+      <Title>
+        <Text>Contact</Text>
+        <Line />
+      </Title>
+      <Content>
+        <Info></Info>
+      </Content>
+    </Wrap>
   );
 };
 
@@ -17,6 +20,7 @@ const Wrap = styled.main`
   width: 100%;
   height: 100vh;
   display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.div`
@@ -25,9 +29,24 @@ const Title = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-`
+
+  @media screen and (max-width: 1320px) {
+    width: 320px;
+  }
+
+  @media screen and (max-width: 850px) {
+    width: 240px;
+  }
+
+  @media screen and (max-width: 410px) {
+    width: 180px;
+  }
+  @media screen and (max-width: 410px) {
+    width: 150px;
+  }
+`;
 const Text = styled.span`
-  font-size : 80px;
+  font-size: 80px;
   color: #1f485e;
   font-weight: bold;
   -webkit-text-stroke: 2px white;
@@ -35,12 +54,54 @@ const Text = styled.span`
   margin-top: 50px;
   padding-right: 30px;
   letter-spacing: 3px;
+
+  @media screen and (max-width: 1320px) {
+    font-size: 50px;
+    margin-top: 20px;
+  }
+  @media screen and (max-width: 850px) {
+    -webkit-text-stroke: 1px white;
+    margin-top: 10px;
+    font-size: 40px;
+    padding-right: 15px;
+  }
+
+  @media screen and (max-width: 410px) {
+    font-size: 25px;
+    color: white;
+    font-weight: normal;
+  }
 `;
 
 const Line = styled.div`
   width: 380px;
   height: 10px;
   margin-top: 15px;
-  margin-bottom: 15px;
+  margin-bottom: 25px;
   border-bottom: 3px solid white;
-  `;
+
+  @media screen and (max-width: 850px) {
+    margin-top: 5px;
+    margin-bottom: 15px;
+  }
+
+  @media only screen and (max-height: 770px) {
+    margin-top: 5px;
+    margin-bottom: 10px;
+  }
+`;
+
+const Content = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Info = styled.section`
+  width: 60%;
+  height: 70%;
+  background-color: white;
+`;
