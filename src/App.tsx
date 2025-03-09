aa3b33dfc9b0d11a77f62dbd235cc4950606899d
jihyeon <104323906+chochojj@users.noreply.github.com>
2025-03-09 23:18:50 +0900

    포트폴리오 다시 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,12 @@
-import styled from "styled-components";
-import Intro from "./pages/Intro";
-import About from "./pages/About";
-import Project from "./pages/Project";
-import Contact from "./pages/Contact";
-import PinkDot from "./component/Pinkdot";
-import { SectionsContainer, Section } from "react-fullpage";
+import "./style/global.css"
 
 function App() {
-  const options = {
-    activeClass: "active", // the class that is appended to the sections links
-    anchors: ["Intro", "About", "Project", "Contact"], // the anchors for each sections
-    arrowNavigation: false, // use arrow keys
-    delay: 1200, // the scroll animation speed
-    navigation: true, // use dots navigatio
-    scrollBar: false, // use the browser default scrollbar
-  };
+
   return (
-    <Wrap>
-      <PinkDot />
-      <Main>
-        <SectionsContainer {...options}>
-          <Section>
-            <Intro />
-          </Section>
-          <Section>
-            <About />
-          </Section>
-          <Section>
-            <Project />
-          </Section>
-          <Section>
-            <Contact />
-          </Section>
-        </SectionsContainer>
-      </Main>
-    </Wrap>
+    <section className="mx-auto text-gray-400">
+      qwef
+    </section>
   );
 }
 
 export default App;
-
-const Wrap = styled.main`
-  width: 100%;
-  display: flex;
-  cursor: none;
-`;
-
-const Main = styled.article`
-  width: 100%;
-`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Reset } from "styled-reset";
-import GlobalStyle from "./style/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
     <Reset />
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
